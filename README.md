afbdb11af7b59cc7093cc0ad6346ca4d0ef6e40c
ArthurMelo777 <meloarthurmelo@gmail.com>
2022-03-19 17:37:40 -0300

    Atualização do README.md e upload da atividade 2374 em python

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # beerowd
 Atividades realizadas no beecrowd
+
+link de acesso: https://www.beecrowd.com.br/judge/pt/recommendations/index/1
---
+++ atividadesPython/2374.py
@@ -0,0 +1,7 @@
+def difPressao(p1, p2):
+    return p1-p2
+
+pressao1 = int(input())
+pressao2 = int(input())
+
+print(difPressao(pressao1, pressao2))
